64ea1cc85db878fbe1938faf160528ea80508ef1
Vivek Namdev <viveknamdev257@gmail.com>
2024-12-18 16:00:46 +0530

    created sidebar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,11 +4,14 @@ import { Cards } from "./components/Cards"
 import { CreateContentModal } from "./components/CreateContentModal"
 import { PlusIcon } from "./icons/PlusIcon"
 import { ShareIcon } from "./icons/ShareIcon"
+import { Sidebar } from "./components/Sidebar"
 
 function App() {
   const [modalOpen, setModalOpen] = useState(true);
   return (
-    <div className="p-4">
+    <div>
+      <Sidebar/>
+      <div className="p-4 ml-72 min-h-screen bg-gray-100 border-2">
       <CreateContentModal open={modalOpen} onClose={() => {
         setModalOpen(false);
       }}/>
@@ -23,6 +26,7 @@ function App() {
       link="https://x.com/AllAboutEvents8/status/1868976679697486330"/>
       <Cards title="first video" type="youtube" 
       link="https://www.youtube.com/watch?v=gGHaXVO-9j4&t=9222s&ab_channel=HarkiratSingh"/>
+      </div>
       </div>
     </div>
   )
---
+++ frontend/src/components/Sidebar.tsx
@@ -0,0 +1,5 @@
+export function Sidebar() {
+    return <div className="h-screen bg-white border-r w-72 fixed left-0 top-0">
+        Hi There
+    </div>
+}
